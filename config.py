d9f1049292052f10496f85437a9fa6fac1e2aebd
nyomanjyotisa <jyotisa1616@gmail.com>
2021-04-08 19:47:27 +0800

    config cloud

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 DELAY = 1
 
 DB_TOKO = {
-    'host': 'localhost',
-    'user': 'root',
-    'pass': '',
+    'host': 'ls-4d24bcf5a512ee7a85643848c6dc9003e0c79f05.cracrx4tayxk.ap-southeast-1.rds.amazonaws.com',
+    'user': 'dbmasteruser',
+    'pass': 'gif;^^<N|JEJBw_mQNs726,Z&xn2dU~_',
     'db_name': 'db_toko'
 }
 
 DB_BANK = {
-    'host': 'localhost',
-    'user': 'root',
-    'pass': '',
+    'host': 'ls-4d24bcf5a512ee7a85643848c6dc9003e0c79f05.cracrx4tayxk.ap-southeast-1.rds.amazonaws.com',
+    'user': 'dbmasteruser',
+    'pass': 'gif;^^<N|JEJBw_mQNs726,Z&xn2dU~_',
     'db_name': 'db_bank'
 }
